76a5b6054282a21bc7a6245ea33a36795393744e
Lee Supe <supelee@amazon.com>
2015-06-30 11:34:43 -0700

    Updated submodules.sh to use "git submodule update --init --recursive".

--- submodules.sh
+++ submodules.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-for dir in `find bundle -maxdepth 1 -type d`
-do
-   pushd "$dir"
-   git submodule init
-   git submodule update
-   popd
-done
+git submodule update --init --recursive
 
